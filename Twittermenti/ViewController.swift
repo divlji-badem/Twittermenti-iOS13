8a77386e72893ab910961bae76dd698f6c4a0cce
Jelena Tasic <jelenatasicsd@gmail.com>
2021-11-02 20:39:56 +0100

    Get data from twitter.

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -1,26 +1,39 @@
 //
 //  ViewController.swift
-//  Twittermenti
-//
-//  Created by Angela Yu on 17/07/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
+
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
-
+    
+    var swifter: Swifter?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if  let path = Bundle.main.path(forResource: "Secrets", ofType: "plist"),
+            let xml = FileManager.default.contents(atPath: path),
+            let secrets = try? PropertyListDecoder().decode(Secrets.self, from: xml)
+        {
+            swifter = Swifter(consumerKey: secrets.APIKey, consumerSecret: secrets.APISecret)
+        }
+        // we get just 15 tweets back, default count is 15 maximum is 100 (more tweets we have to perform this search more than onde increasing the date range or use premium plans
+        swifter?.searchTweet(using: "@Apple", count: 100, success: { results, metadata in
+            print(results)
+        }, failure: { error in
+            print("There was an error with Twitter API request, \(error)")
+        })
+
     }
 
     @IBAction func predictPressed(_ sender: Any) {
-    
+
     
     }
     
