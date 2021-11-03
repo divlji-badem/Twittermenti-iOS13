307b688082291f1f13f0db2624937ddd6f5cef0d
Jelena Tasic <jelenatasicsd@gmail.com>
2021-11-03 10:28:11 +0100

    Getting score of all 100 tweets.

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -2,10 +2,10 @@
 //  ViewController.swift
 //
 
-
 import UIKit
 import SwifteriOS
 import CoreML
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
@@ -34,19 +34,42 @@ class ViewController: UIViewController {
         {
             swifter = Swifter(consumerKey: secrets.APIKey, consumerSecret: secrets.APISecret)
         }
-        // we get just 15 tweets back, default count is 15 maximum is 100 (more tweets we have to perform this search more than onde increasing the date range or use premium plans
-        // lang - just tweets on specific language
-        // extended mode - to see full text
-        swifter?.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { results, metadata in
+        /* we get just 15 tweets back, default count is 15 maximum is 100 (more tweets we have to perform this search more than onde increasing the date range or use premium plans
+         lang - just tweets on specific language
+         extended mode - to see full text */
+        
+        swifter?.searchTweet(using: "#Apple", lang: "en", count: 100, tweetMode: .extended, success: { results, metadata in
+            
             //print(results)
+            var tweets = [TweetSentimentClassifierInput]()
+            for i in 0..<100 {
+                if let tweet = results[i]["full_text"].string {
+                    let tweetForClassification  = TweetSentimentClassifierInput(text: tweet)
+                    tweets.append(tweetForClassification)
+                }
+            }
+            
+            do {
+                let predictions = try self.model.predictions(inputs: tweets)
+                var sentimentScore = 0
+                for prediction in predictions {
+                    let sentiment = prediction.label
+                    if sentiment == "Pos" {
+                        sentimentScore += 1
+                    } else if sentiment == "Neg" {
+                        sentimentScore -= 1
+                    }
+                }
+                print("\(sentimentScore)")
+               
+            } catch {
+                print("There was an error with making a prediction, \(error)")
+            }
+            
         }, failure: { error in
             print("There was an error with Twitter API request, \(error)")
         })
  
-        let prediction = try! model.prediction(text: "@Apple is the best company.")
-        print(prediction.label)
-
-
     }
 
     @IBAction func predictPressed(_ sender: Any) {
